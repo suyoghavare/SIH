9c0150375a7e016c81e93dc15e4533d633240fd9
suyoghavare <suyoghavare@gmail.com>
2023-09-11 13:40:18 +0530

    sms done

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,62 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { SendIntentAndroid } from 'react-native-sms';
 import Communications from 'react-native-communications';
-import { useNavigation } from '@react-navigation/native';
+import queryString from 'query-string';
+import { NativeModules,PermissionsAndroid} from 'react-native';
+
+let DirectSms = NativeModules.DirectSms;
 
 const PaymentScreen = () => {
-  const [paymentInProgress, setPaymentInProgress] = useState(false);
   const [recipientName, setRecipientName] = useState('');
   const [amount, setAmount] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [scanned, setScanned] = useState(false);
-  const [qrScannerActive, setQrScannerActive] = useState(false);
-
-  const navigation = useNavigation();
-
-  const handleScan = ({ data }) => {
-    setScanned(true);
-    setQrScannerActive(false);
-    promptForAmountAndPin(data);
-  };
-
-  const promptForAmountAndPin = (qrData) => {
-    // Show a custom modal or dialog to prompt for amount and PIN
-    // Once the user enters the amount and PIN, send the SMS
-    // Example:
-    // You can use a library like 'react-native-modal' to show a modal
-
-    // Construct the message
-    const message = `Scanned QR code data: ${qrData}\nAmount: ${amount}\nPIN: ${enteredPin}`;
-    const recipients = '9892731267'; // Replace with the recipient's phone number
-    Communications.text(recipients, message);
-    // Send SMS
-
-    // Navigate back to the payment screen
-    navigation.navigate('Payment');
-  };
-
-  const handleBackFromScanner = () => {
-    setQrScannerActive(false);
-    navigation.navigate('Payment');
-  };
-
-  useEffect(() => {
-    // Request permissions and set hasPermission state here
-  }, []);
-
+  const mobileNumber = '9892731267';
   const handlePayment = () => {
-    setPaymentInProgress(true);
-    sendPaymentDataViaSMS();
-    setTimeout(() => {
-      setPaymentInProgress(false);
-    }, 3000);
+    // Construct the message
+    if (!recipientName || !amount || !phoneNumber) {
+      Alert.alert('Error', 'Please fill in all fields.');
+      return;
+    }
+    
+    // const message = `Recipient's Name: ${recipientName}, Amount: ${amount}, Phone Number: ${phoneNumber}`;
+    //         const recipients = '9892731267'; // Replace with the recipient's phone number
+          
+    //         Communications.textWithoutEncoding(recipients, message);
+
   };
 
-  const sendPaymentDataViaSMS = () => {
-    const message = `Recipient's Name: ${recipientName}, Amount: ${amount}, Phone Number: ${phoneNumber}`;
-    const recipients = '9892731267'; // Replace with the recipient's phone number
-    Communications.text(recipients, message);
-    navigation.navigate('PaymentSuccess');
+  const sendDirectSms = async () => {
+    if (mobileNumber) {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.SEND_SMS,
+          {
+            title: 'Send SMS App Sms Permission',
+            message:
+              'Send SMS App needs access to your inbox ' +
+              'so you can send messages in background.',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          DirectSms.sendDirectSms(mobileNumber, `${recipientName},${amount},${phoneNumber}`);
+          alert('SMS sent');
+        } else {
+          alert('SMS permission denied');
+        }
+      } catch (error) {
+        console.warn(error);
+        alert(error);
+      }
+    }
   };
 
   return (
@@ -91,46 +86,9 @@ const PaymentScreen = () => {
           onChangeText={setPhoneNumber}
         />
       </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handlePayment}
-        disabled={paymentInProgress}
-      >
-        <Text style={styles.buttonText}>
-          {paymentInProgress ? 'Payment in Process...' : 'Make Payment'}
-        </Text>
+      <TouchableOpacity style={styles.button} onPress={sendDirectSms}>
+        <Text style={styles.buttonText}>Make Payment</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.qrContainer}>
-        {qrScannerActive ? (
-          <TouchableOpacity
-            style={styles.backButton}
-            onPress={handleBackFromScanner}
-          >
-            <Text style={styles.backButtonText}>Back</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            style={styles.qrButton}
-            onPress={() => {
-              setScanned(false);
-              setQrScannerActive(true);
-            }}
-            disabled={scanned}
-          >
-            <Text style={styles.qrButtonText}>
-              {scanned ? 'QR Scanned' : 'Scan QR'}
-            </Text>
-          </TouchableOpacity>
-        )}
-      </TouchableOpacity>
-
-      {/* QR Scanner View */}
-      {qrScannerActive && (
-        /* Include your QR scanner component here */
-        <View style={styles.qrScanner}>
-          {/* Add QR Scanner UI */}
-        </View>
-      )}
     </View>
   );
 };
@@ -145,6 +103,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    color: 'black',
   },
   inputContainer: {
     marginBottom: 20,
@@ -172,34 +131,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
-  },
-  qrContainer: {
-    position: 'absolute',
-    bottom: 20,
-    alignSelf: 'center',
-  },
-  qrButton: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 30,
-  },
-  backButton: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 30,
-  },
-  backButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  qrButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  qrScanner: {
-    flex: 1, // Adjust as needed
   },
 });
 
